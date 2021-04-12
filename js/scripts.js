b58fdc33108249da1d42ca3f86a441052e5b32a9
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-04-12 15:50:06 -0700

    NPM Scripts Part 1, for SCSS files and updates

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".carousel").carousel( {interval: 4500, pause: "false"} );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        }   else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
