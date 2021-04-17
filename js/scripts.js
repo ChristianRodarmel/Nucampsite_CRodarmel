97b4289f1d229dadfdd9180e9a65c180c623995b
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-04-17 13:00:45 -0700

    Modal changes to JQuery/scripts added

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,11 @@ $(function() {
         }
     });
 });
+
+$('#reserveButton').click(function(){
+    $('#reserveModal').modal('show');
+});
+
+$('#loginButton').click(function(){
+    $('#loginModal').modal('show');
+});
